48a9d72fcb080b20725e97062c91d252ef6dc085
AzzelyaRosya <azzelya.denovvya@gmail.com>
2022-03-22 10:40:15 +0700

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import { Typography, Card, Avatar, Grid } from '@mui/material';
 export default function Contact(props) {
   return (
     <>
-      <Card sx={{ border: 2, borderColor: '#d9d9d9', maxWidth: 5000, my: 1, p: 2 }} style={{ backgroundColor: '#e6e6e6' }}>
+      <Card sx={{ border: 2, borderColor: '#fff', maxWidth: 5000, my: 1, p: 2 }}>
         <Grid container wrap="nowrap" spacing={2}>
           <Grid item>
             <Avatar src={props.img}></Avatar>
